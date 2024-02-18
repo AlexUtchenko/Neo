3206138db0dab1b65082d5fecf1d06fb21ae0d7a
AlexUtchenko <87366032+AlexUtchenko@users.noreply.github.com>
2024-02-18 14:23:02 +0200

    HW3

---
+++ Algorithms/HW3/HW3_1.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+from collections import defaultdict
+
+
+TRANSFER_DICT = defaultdict(lambda: [])
+FOLDERS = []
+
+
+def get_extensions(file_name) -> str:
+    return Path(file_name).suffix[1:].upper()
+
+
+def scan(folder: Path):
+    """Функції сканує рекурсивно директорії в глибину, починаючи від заданої. Вносить перелік файлів за ключем їх розширеня в словник переносу і список тек для видалення"""
+    for item in folder.iterdir():
+        if item.is_dir():
+            FOLDERS.append(item)
+            scan(
+                item
+            )  # рекурсивний виклик функції для заглиблення на рівень нижче в файловій системі
+        else:
+            # наповнення словника переносу
+            extension = get_extensions(item.name)
+            new_name = folder / item.name
+            if not extension:
+                TRANSFER_DICT["NON_EXTENSION"].append(new_name)
+            else:
+                TRANSFER_DICT[extension].append(new_name)
+
+
+def transfer_file(file: Path, root_folder: Path, destination: str):
+    """Функція, яка реалізує перенос файлів"""
+
+    target_folder = Path(destination)
+    target_folder.mkdir(exist_ok=True)
+    file.replace(target_folder / file.name)
+
+
+def delete_folder(folder: Path):
+    """Функція видалення тек"""
+    try:
+        folder.rmdir()
+    except OSError:
+        print("Помилка видалення текі")
+
+
+def main(folder: Path, destination: Path):
+    """Основна логіка програми"""
+
+    # сканування вибраної частини файлової системи, наповнення словника переносу і списку видалення
+    scan(folder)
+    # реалізація переносу файлів за словником переносу
+    for ext, files in TRANSFER_DICT.items():
+        for file in files:
+            dest = destination / ext
+            transfer_file(file, folder, dest)
+    # видалення тек
+    for f in FOLDERS:
+        delete_folder(f)
+
+
+if __name__ == "__main__":
+    # вітання
+    print(f'\n{"*"*10:^50}Welcome to sorter app!{"*"*10:^50}\n')
+    # ввод, обробка і валідацію шляхів, введених користувачем
+    while True:
+        folder = Path(input("Введіть теку, яку треба відсортувати \n>>> "))
+        if folder.is_dir():
+            break
+        else:
+            print("!!! Введіть вірний шлях до текі !!!")
+    while True:
+        try:
+            destination = input("Введіть теку, куди треба зберігти \n>>> ")
+            if not destination:
+                destination = Path.cwd() / "dist"
+            else:
+                destination = Path(destination)
+            destination.mkdir()
+            break
+        except:
+            print("!!! Введіть вірний шлях до текі призначення !!!")
+    if not destination.is_absolute():
+        print(f"Введено відносний шлях:\n {Path.cwd() / destination.name}")
+    # реалізація основної логіки на веріфікованих шляхаха
+    main(folder, destination)
+    # завершення програми
+    print(
+        f"Сортування виконанно! Шукайте відсортовані по текам файли за наступним шляхом:\n{destination}"
+    )
---
+++ Algorithms/HW3/HW3_2.py
@@ -0,0 +1,35 @@
+import turtle
+
+def koch_snowflake(turtle, length, depth):
+  """
+  Функція для малювання сніжинки Коха рекурсивно
+    length: Довжина сторони трикутника
+    depth: Глибина рекурсії
+  """
+
+  if depth == 0:
+    turtle.forward(length)
+  else:
+    koch_snowflake(turtle, length / 3, depth - 1)
+    turtle.left(60)
+    koch_snowflake(turtle, length / 3, depth - 1)
+    turtle.right(120)
+    koch_snowflake(turtle, length / 3, depth - 1)
+    turtle.left(60)
+    koch_snowflake(turtle, length / 3, depth - 1)
+
+# Створення нового об'єкта turtle
+t = turtle.Turtle()
+
+# Встановлення параметрів
+t.speed(0)
+t.pencolor("black")
+t.fillcolor("lightblue")
+
+# Малювання сніжинки Коха на 3 сторонах рівностороннього трикутника з кутом повороту 180-60=120 градусів
+for i in range(3): # кількість сторін (3)
+  koch_snowflake(t, 300, 4)
+  t.right(120) # кут повороту (120)
+
+# Завершення роботи turtle
+turtle.done()
